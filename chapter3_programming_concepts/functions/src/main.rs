e93d41ec4146cb9e5d7967c11f926e1fc94b163d
Calum Price - Work <calumprice.dev@gmail.com>
2025-12-04 10:39:13 +0000

    function return and parameters

--- chapter3_programming_concepts/functions/src/main.rs
+++ chapter3_programming_concepts/functions/src/main.rs
@@ -6,12 +6,19 @@ fn main() {
     another_function_with_paramater(5);
     print_labeled_measurement(5, 'h'); // char should be in '' (single quotes)
 
-    let _y = 6; // this is a statement
+    let _y = 6; // this is a statement. adding semi colon to an expression would make it a
+                // statement
 
     let _x = {
         let y = 3;
         y + 1
     }; // this is an expression within a statement
+
+    let five = five(); // using the return value of the function the same as let five = 5;
+    println!("the value of five is: {five}");
+
+    let six = plus_one(five); // takes an i32 as an argument and returns an i32
+    println!("the value of five is: {six}");
 }
 
 // could be placed before the main function. you can define functions anywhere as long as they are
@@ -28,4 +35,12 @@ fn another_function_with_paramater(x: i32) {
 // multiple params in the signature are seperated by commas
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("the measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5 // this is an expression that returns the value 5 (i32)
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 // adds one to the value of x passed as an argument to the function call
 }
