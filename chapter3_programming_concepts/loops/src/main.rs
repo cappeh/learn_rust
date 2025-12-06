001e5f5cdb01f00fe24c5c05cb1439a6ad29d4d2
Calum Price <calumprice.dev@gmail.com>
2025-12-06 13:53:58 +0000

    loop label

--- chapter3_programming_concepts/loops/src/main.rs
+++ chapter3_programming_concepts/loops/src/main.rs
@@ -1,15 +1,21 @@
 fn main() {
-    // in the following code Ctrl-C will need to be used to interrupt the program
-    // loop {
-    //     println!("again");
-    // }
-    let mut counter = 0; // declare and initialize counter
+    let mut count = 0;
 
-    let result = loop { // result will hold the value returned from the loop
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
         }
-    };
-    println!("The result is: {result}"); // will print 20
+        count += 1;
+    }
+    println!("End count = {count}");
 }
