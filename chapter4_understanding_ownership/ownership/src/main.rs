744c85d470a8c2056f791df68d13df1ea6bbfb60
Calum Price <calumprice.dev@gmail.com>
2025-12-10 19:44:15 +0000

    more on ownership with functions (scope and return values)

--- chapter4_understanding_ownership/ownership/src/main.rs
+++ chapter4_understanding_ownership/ownership/src/main.rs
@@ -38,4 +38,42 @@ fn main() {
     let mut s3 = String::from("Hello");
     s3 = String::from("Ahoy"); // drop will be called on the initial value "hello" to free memory
     println!("{s3}, World!");
+
+    let s4 = String::from("Some String"); // comes into scope
+    takes_ownership(s4); // s4 moves into takes_ownership and no longer available in this scope
+    // println!("{s4}"); this causes an error because s4 has moved to takes_ownership and no longer
+    // available in this scope
+
+    makes_copy(x); // i32 implements the copy trait so does not move into makes_copy
+    println!("{x} from the main function"); // x can be used afterwards because of the Copy trait
+
+    let _s5 = gives_ownership(); // return value from gives_ownership is moved into _s5
+    let s6 = String::from("hello"); // comes into scope
+    let s7 = takes_and_gives_back(s6); // s6 is moved into this function and its return value is
+                                        // moved to _s7
+
+    let (_s8, _len) = calculates_length(s7);
+}
+
+fn takes_ownership(some_string: String) { // some string comes into scope
+    println!("{some_string}");
+} // some_string goes out of scope and Drop is called and memory is freed
+
+fn makes_copy(some_integer: i32) {
+    println!("{some_integer}");
+}
+
+fn gives_ownership() -> String { // some_string is moved into the calling function
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into scope and is moved to the calling function
+    a_string 
+}
+
+fn calculates_length(s: String) -> (String, usize) { // returns a tuple of String and usize
+    let length = s.len();
+    (s, length) // returns the string and length of the string
 }
