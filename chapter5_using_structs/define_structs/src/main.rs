605fe310cf4af2d93968ca90c334814cd487f523
Calum Price <calumprice.dev@gmail.com>
2025-12-13 18:57:36 +0000

    instantiating structs

---
+++ chapter5_using_structs/define_structs/src/main.rs
@@ -0,0 +1,21 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User { active: false, username: username, email: email, sign_in_count: 1 }
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    let user2 = build_user(String::from("user2@example.com"), String::from("username2"));
+}
