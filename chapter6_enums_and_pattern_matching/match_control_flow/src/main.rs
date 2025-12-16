5db5e29cb43da43f69921a90cd05fa2b20964982
Calum Price <calumprice.dev@gmail.com>
2025-12-16 19:47:16 +0000

    getting the inner values of patterns

--- chapter6_enums_and_pattern_matching/match_control_flow/src/main.rs
+++ chapter6_enums_and_pattern_matching/match_control_flow/src/main.rs
@@ -1,20 +1,49 @@
 #[allow(unused)]
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+#[allow(unused)]
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky Penny");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}");
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
     println!("The value of a Dime is: {}", value_in_cents(Coin::Dime));
+    println!("The value of a Penny is: {}", value_in_cents(Coin::Penny));
+    println!("The value of a Penny is: {}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{six:?}");
+    println!("{none:?}");
 }
