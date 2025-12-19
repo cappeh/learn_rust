1191a1c87adb70c3415b02b0de02f607ca12e487
Calum Price <calumprice.dev@gmail.com>
2025-12-19 16:35:28 +0000

    modules

---
+++ chapter7_packages_crates_modules/restaurant/target/debug/deps/restaurant-09b18eb38fd4904d.d
@@ -0,0 +1,5 @@
+/home/cappie/learn_rust/chapter7_packages_crates_modules/restaurant/target/debug/deps/restaurant-09b18eb38fd4904d.d: src/lib.rs
+
+/home/cappie/learn_rust/chapter7_packages_crates_modules/restaurant/target/debug/deps/librestaurant-09b18eb38fd4904d.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ chapter7_packages_crates_modules/restaurant/target/debug/deps/restaurant-efd004d69a01b3b2.d
@@ -0,0 +1,5 @@
+/home/cappie/learn_rust/chapter7_packages_crates_modules/restaurant/target/debug/deps/restaurant-efd004d69a01b3b2.d: src/lib.rs
+
+/home/cappie/learn_rust/chapter7_packages_crates_modules/restaurant/target/debug/deps/librestaurant-efd004d69a01b3b2.rmeta: src/lib.rs
+
+src/lib.rs:
