95574e1659f4e5231b133e370615341c378c8434
Calum Price <calumprice.dev@gmail.com>
2025-12-21 14:26:01 +0000

    more on paths and privacy rules

--- chapter7_packages_crates_modules/restaurant/src/lib.rs
+++ chapter7_packages_crates_modules/restaurant/src/lib.rs
@@ -1,6 +1,7 @@
+#[allow(dead_code)]
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
 
@@ -10,3 +11,39 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
+
+#[allow(dead_code)]
+fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("{meal:?}");
+}
+
+#[allow(dead_code)]
+fn deliver_order() {}
+
+mod back_of_house {
+    #[allow(dead_code)]
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { toast: String::from(toast), seasonal_fruit: String::from("peaches") }
+        }
+    }
+}
+
