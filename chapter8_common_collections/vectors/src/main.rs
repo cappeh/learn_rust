0fc7951e266153cd3ac0bfb86ead6bbe52c5a240
Calum Price - Work <calumprice.dev@gmail.com>
2025-12-22 14:28:52 +0000

    the vector collection

---
+++ chapter8_common_collections/vectors/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut v1: Vec<i32> = Vec::new();
+    let v2 = vec![1, 2, 3];
+
+    v1.push(4);
+    v1.push(5);
+    v1.push(6);
+
+    println!("Vector1 With Type Annotation: {v1:?}");
+    println!("Vector2 With Macro Definition: {v2:?}");
+
+    let v3 = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v3[2];
+    println!("The third element is: {third}");
+
+    let third: Option<&i32> = v3.get(2);
+    match third {
+        Some(third) => println!("The third element using get is: {third}"),
+        None => println!("There is no third element"),
+    }
+
+    for i in &v3 {
+        println!("{i}");
+    }
+
+    let mut v4 = vec![12, 34, 87, 109, 172];
+    
+    for i in &mut v4 {
+        *i += 50;
+    }
+
+    println!("New v4 values: {v4:?}");
+
+    {
+        let _v5 = vec![10, 20, 30];
+    } // v5 and all its values are dropped when the scope ends
+}
