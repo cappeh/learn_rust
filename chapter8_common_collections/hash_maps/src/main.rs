9743ab789afdf7dbcbc55635e2063ec86c2cbd66
Calum Price <calumprice.dev@gmail.com>
2025-12-24 18:18:02 +0000

    updating hash maps

--- chapter8_common_collections/hash_maps/src/main.rs
+++ chapter8_common_collections/hash_maps/src/main.rs
@@ -26,4 +26,14 @@ fn main() {
     for (key, val) in &scores {
         println!("{key}: {val}");
     }
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
 }
