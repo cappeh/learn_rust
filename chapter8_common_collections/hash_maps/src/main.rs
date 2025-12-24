7d272c6d4efe0f57ad12f1533c2cf750cbfa5913
Calum Price <calumprice.dev@gmail.com>
2025-12-24 15:42:25 +0000

    creating, accessing and managing ownership of a HashMap

---
+++ chapter8_common_collections/hash_maps/src/main.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{scores:?}");
+
+    let blue_team_name = String::from("Blue");
+    // copied() takes Option<&i32> and returns Option<i32>
+    // gets T from &T
+    let blue_score = scores.get(&blue_team_name).copied().unwrap_or(0);
+    println!("{blue_score}");
+
+    let _yellow_team_name = String::from("Yellow");
+    
+    // the below returns Option<&i32> so unwrap_or would also need a reference
+    let yellow_score = scores.get("Yellow").unwrap_or(&0);
+    println!("{yellow_score:?}");
+
+    // this will overwrite the value associated with the key "Yellow"
+    scores.insert(String::from("Yellow"), 9);
+
+
+    for (key, val) in &scores {
+        println!("{key}: {val}");
+    }
+}
