7d3619bc3204c599c9476dc42ddc09bd977aa6cd
Calum Price <calumprice.dev@gmail.com>
2025-12-27 19:13:00 +0000

    propagating errors

--- chapter9_error_handling/recoverable_errors/src/main.rs
+++ chapter9_error_handling/recoverable_errors/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::ErrorKind};
+use std::{fs::File, io::{self, ErrorKind, Read}};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
@@ -35,4 +35,24 @@ fn main() {
     // in the Err value
     let _greeting_file_expect = File::open("hello.txt").expect("hello.txt not in project");
 
+}
+
+#[allow(dead_code)]
+// function will return Result<String, io::Error> T is String, E is io::Error
+// if the function succeeds without issues, the code that calls this function gets Ok(String)
+// containing the username
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e), // return early out of the function returning the error "e"
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e), // dont need to explicitly use "return" as this is the last expression
+    }
 }
