242d9dfedc3c466a3d7fe952696184e2c4389fd7
Calum Price <calumprice.dev@gmail.com>
2025-12-27 20:04:47 +0000

    using the ? operator

--- chapter9_error_handling/recoverable_errors/src/main.rs
+++ chapter9_error_handling/recoverable_errors/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{self, ErrorKind, Read}};
+use std::{fs::{self, File}, io::{self, ErrorKind, Read}};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
@@ -55,4 +55,24 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e), // dont need to explicitly use "return" as this is the last expression
     }
+}
+
+#[allow(dead_code)]
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+#[allow(dead_code)]
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+#[allow(dead_code)]
+fn read_username_from_file4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
