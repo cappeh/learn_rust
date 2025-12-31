ddb141bb772b8c516c6edc781e578165b0dd6108
Calum Price <calumprice.dev@gmail.com>
2025-12-31 19:27:24 +0000

    generics on structs and when impl structs

--- chapter10_generics_traits_lifetimes/generics/src/main.rs
+++ chapter10_generics_traits_lifetimes/generics/src/main.rs
@@ -20,6 +20,30 @@
 //     largest
 // }
 
+#[allow(dead_code)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f64> {
+    fn x_and_y(self) -> (f64, f64) {
+        (self.x, self.y)
+    }
+}
+
+#[allow(dead_code)]
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
@@ -41,4 +65,11 @@ fn main() {
     // let result = largest_char(&char_list);
     let result = largest(&char_list);
     println!("the largest char is {result}");
+
+    let p1 = Point { x: 5, y: 10 };
+    println!("p1 x value is: {}", p1.x());
+
+    let p3 = Point { x: 3.2, y: 4.2 };
+    let (left, right) = p3.x_and_y();
+    println!("{left}, {right}")
 }
