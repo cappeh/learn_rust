45038a1e89cb90e686cdb8e8bccc18e1b76661fc
Calum Price <calumprice.dev@gmail.com>
2025-12-31 10:41:16 +0000

    after removing code duplication

--- chapter10_generics_traits_lifetimes/code_duplication/src/main.rs
+++ chapter10_generics_traits_lifetimes/code_duplication/src/main.rs
@@ -1,22 +1,36 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-
-    let mut largest = &number_list[0];
-
-    for num in &number_list {
-        if num > largest {
-            largest = num;
-        }
-    }
-    println!("the largest number is: {largest}");
-
+    let result = largest(&number_list);
+    println!("the largest number is: {result}");
+    // let mut largest = &number_list[0];
+    //
+    // for num in &number_list {
+    //     if num > largest {
+    //         largest = num;
+    //     }
+    // }
+    // println!("the largest number is: {largest}");
+    //
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let mut largest = &number_list[0];
+    let result = largest(&number_list);
+    println!("the largest number is: {result}");
+    // let mut largest = &number_list[0];
+    //
+    // for num in &number_list {
+    //     if num > largest {
+    //         largest = num;
+    //     }
+    // }
+    // println!("the largest number is {largest}");
+}
 
-    for num in &number_list {
-        if num > largest {
-            largest = num;
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
-    println!("the largest number is {largest}");
+    largest
 }
