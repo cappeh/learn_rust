3a5ab1b300c2e385fada4698e5758a3500ca5055
Calum Price <calumprice.dev@gmail.com>
2025-12-31 15:04:59 +0000

    creating a generic largest function

--- chapter10_generics_traits_lifetimes/generics/src/main.rs
+++ chapter10_generics_traits_lifetimes/generics/src/main.rs
@@ -1,15 +1,26 @@
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+// fn largest_i32(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+//
+// fn largest_char(list: &[char]) -> &char {
+//     let mut largest = &list[0];
+//
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -22,10 +33,12 @@ fn largest_char(list: &[char]) -> &char {
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    // let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("the largest number is {result}");
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    // let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("the largest char is {result}");
 }
