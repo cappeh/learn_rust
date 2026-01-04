80f03462f2ef1c573fbf5f394026429ac4405bb0
Calum Price <calumprice.dev@gmail.com>
2026-01-04 16:13:38 +0000

    code with lifetimes annotated

--- chapter10_generics_traits_lifetimes/lifetimes/src/main.rs
+++ chapter10_generics_traits_lifetimes/lifetimes/src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     println!("the longest string is: {result}");
 }
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {x} else {y}
 }
