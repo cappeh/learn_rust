53ee840fb43c502b416ad44e56867ca9ea03c013
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-05 14:17:33 +0000

    an example of a return reference matching one of its parameters

--- chapter10_generics_traits_lifetimes/lifetimes/src/main.rs
+++ chapter10_generics_traits_lifetimes/lifetimes/src/main.rs
@@ -1,14 +1,23 @@
 fn main() {
     let string1 = String::from("long string is long");
+    let x;
 
     {
         let string2 = String::from("xyz");
 
         let result = longest(string1.as_str(), string2.as_str());
         println!("the longest string is: {result}");
+
+        x = return_x(string1.as_str(), string2.as_str());
     }
+
+    println!("x is: {x}");
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {x} else {y}
+}
+
+fn return_x<'a>(x: &'a str, _y: &str) -> &'a str {
+    x
 }
