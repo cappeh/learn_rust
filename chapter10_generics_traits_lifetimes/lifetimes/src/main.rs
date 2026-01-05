bd633018db2081f3f64033178e3f83ed34633701
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-05 15:52:27 +0000

    lifetimes in struct definitions

--- chapter10_generics_traits_lifetimes/lifetimes/src/main.rs
+++ chapter10_generics_traits_lifetimes/lifetimes/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+#[allow(dead_code)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("long string is long");
     let x;
@@ -12,6 +18,13 @@ fn main() {
     }
 
     println!("x is: {x}");
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("Novel First Line: {i:?}");
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
