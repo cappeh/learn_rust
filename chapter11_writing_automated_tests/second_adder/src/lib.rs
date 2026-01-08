f364332745ec8feaab2a7a755e7b14f2aadabf51
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-08 15:34:28 +0000

    added additional functions to second_adder to test running subsets of tests

--- chapter11_writing_automated_tests/second_adder/src/lib.rs
+++ chapter11_writing_automated_tests/second_adder/src/lib.rs
@@ -7,8 +7,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_adds_two() {
+    fn check_add_two_and_two() {
         let result = add_two(2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn check_add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn check_add_100_and_two() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
     }
 }
