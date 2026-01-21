d1d2d12bf76374d3b17e68e6fd7ed61cf6e615c4
Calum Price - Work <calumprice.dev@gmail.com>
2026-01-21 10:26:06 +0000

    different ways of writing a closure with and without type annotations

--- chapter13_iterators_and_closures/closure_types/src/main.rs
+++ chapter13_iterators_and_closures/closure_types/src/main.rs
@@ -9,4 +9,17 @@ fn main() {
 
     let closure = expensive_closure(5);
     println!("{closure}");
+
+    let add_one_v2 = |x: u32| -> u32 {x + 1};
+    let add_one_v3 = |x| {x + 1};
+    let add_one_v4 = |x| x + 1;
+
+    println!("{}", add_one_v1(9));
+    println!("{}", add_one_v2(5));
+    println!("{}", add_one_v3(8));
+    println!("{}", add_one_v4(4));
+}
+
+fn add_one_v1(x: u32) -> u32 {
+    x + 1
 }
