2c67e464727c07e8f99325bc543372cc95761450
Calum Price <calumprice.dev@gmail.com>
2026-01-24 14:23:05 +0000

    a test showing a iterator consuming adapter (sum method)

--- chapter13_iterators_and_closures/iterators/src/main.rs
+++ chapter13_iterators_and_closures/iterators/src/main.rs
@@ -20,4 +20,16 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
     }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+
+        // the sum method is a consuming adapter because it calls the nex method of iterators which
+        // then consumes the iterator.
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
 }
