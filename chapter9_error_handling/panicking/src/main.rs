e3e55a23921701a1d5349f2255d239c6f4b90806
Calum Price - Work <calumprice.dev@gmail.com>
2026-02-02 11:21:35 +0000

    using cfg attributes on functions to determine the behaviour when unwind or abort panic strategy is used

--- chapter9_error_handling/panicking/src/main.rs
+++ chapter9_error_handling/panicking/src/main.rs
@@ -1,10 +1,36 @@
+// fn drink(beverage: &str) {
+//     if beverage == "lemonade" {
+//         // ################################################################
+//         // in the Cargo.toml file add the following:
+//         // [profile.dev] ======================= or [profile.release]
+//         // panic = "abort"
+//         // ################################################################
+//         // RUSTFLAGS="-C panic=abort" cargo run (cargo build --release)
+//         if cfg!(panic = "abort") {
+//             println!("this is not your party. Run!!!");
+//         } else {
+//             println!("spit it out!!!");
+//         }
+//     } else {
+//         println!("some refreshing {} is all i need", beverage);
+//     }
+// }
+
+// this function runs when the panic strategy is the default "unwind"
+#[cfg(panic = "unwind")]
+fn ah() {
+    println!("spit it out!!!");
+}
+
+// if the panic strategy is not "unwind" and changed to abort, this function is called
+#[cfg(not(panic = "unwind"))]
+fn ah() {
+    println!("this is not your party. Run!!!");
+}
+
 fn drink(beverage: &str) {
     if beverage == "lemonade" {
-        if cfg!(panic = "abort") {
-            println!("this is not your party. Run!!!");
-        } else {
-            println!("spit it out!!!");
-        }
+        ah();
     } else {
         println!("some refreshing {} is all i need", beverage);
     }
