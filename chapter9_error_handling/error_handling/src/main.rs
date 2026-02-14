5fafe0475b22b643a93c31fcf752e309a9d1706f
Calum Price <calumprice.dev@gmail.com>
2026-02-14 19:50:25 +0000

    manually impl Debug to display error messages

--- chapter9_error_handling/error_handling/src/main.rs
+++ chapter9_error_handling/error_handling/src/main.rs
@@ -3,10 +3,15 @@ use std::fmt;
 
 type FileOpResult<T> = Result<T, FileOpErr>;
 
-#[derive(Debug)]
 struct FileOpErr(io::Error);
 
 impl fmt::Display for FileOpErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Operation Failed With Error: {}", self.0)
+    }
+}
+
+impl fmt::Debug for FileOpErr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Operation Failed With Error: {}", self.0)
     }
@@ -18,18 +23,10 @@ impl From<io::Error> for FileOpErr {
     }
 }
 
-impl std::error::Error for FileOpErr {}
-
-fn run() -> FileOpResult<()> {
+fn main() -> FileOpResult<()> {
     let user = read_username_from_file()?;
     println!("{user}");
     Ok(())
-}
-
-fn main() {
-    if let Err(e) = run() {
-        eprintln!("{}", e);
-    }
 }
 
 fn read_username_from_file() -> FileOpResult<String> {
